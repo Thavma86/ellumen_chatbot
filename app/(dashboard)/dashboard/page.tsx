8ca1658afa7d10bbd2f2ff1c7b7195413872b7af
eleshinlomo <seun.olatunji2@gmail.com>
2023-11-27 06:44:16 -0500

    modifed

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -92,7 +92,7 @@ const DashboardPage = ()=>{
     <div>
        <div className='mb-8 space-y-4'>
         <h2 className='text-2xl md:text-4xl font-bold text-center'>
-        ZEITIOS® Dashboard
+        Ellumen® Dashboard
         </h2>
 
         <p className='text-muted-foreground font-light text-sm md:text-lg text-center'>
